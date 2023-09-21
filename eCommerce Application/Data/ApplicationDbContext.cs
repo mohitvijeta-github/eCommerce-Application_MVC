bf55599556c1d506d524dedd5058a0b0303e218f
mohitvijeta-github <136967178+mohitvijeta-github@users.noreply.github.com>
2023-09-21 13:25:44 +1000

    Created Database Connection

---
+++ eCommerce Application/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace eCommerce_Application.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+            
+        }
+    }
+}
